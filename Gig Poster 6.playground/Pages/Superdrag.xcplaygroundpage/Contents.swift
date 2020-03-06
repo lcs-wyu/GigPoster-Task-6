250a0c44cde8c9eb096a4392c0eeb8f2a7b32ef9
Russell Gordon <rgordon@lcs.on.ca>
2020-03-06 08:44:14 -0500

    Initial Commit

---
+++ Gig Poster 6.playground/Pages/Superdrag.xcplaygroundpage/Contents.swift
@@ -0,0 +1,64 @@
+//: # Gig Poster 6
+//: The following two statements are required to make the playground run. Please do not remove.
+import Cocoa
+import PlaygroundSupport
+
+/*:
+ Each image is exactly 400 pixels wide by 600 pixels tall.
+ 
+ Your goal is to precisely reproduce this image:
+ 
+ ![superdrag-no-grid](superdrag-no-grid.png "Talking Heads")
+ ![superdrag-with-grid](superdrag-with-grid.png "Talking Heads")
+ 
+ By precisely reproducing this image, you will demonstrate your understanding of:
+ 
+ * sequence (order of statements in a program)
+ * conditionals (making decisions with ``if`` statements)
+ * iteration (use of loops to repeat statements)
+ * use of arithmetic operators in expressions
+ * functions
+ 
+ You may use the color constants created below to obtain the correct colors.
+ 
+ Remember that you can refer to the [Canvas class documentation](https://www.russellgordon.ca/canvasgraphics/documentation/classes/canvas), as needed.
+ 
+ ## Use Source Control
+ 
+ To create evidence that supports your case for exceeding expectations for thread 3:
+
+ * You *must* commit and push regularly ... *not* just at the end of this task.
+
+ * You *must* complete your work prior to 1:00 PM on Friday, March 6, 2020.
+ 
+ Good luck! You've got this! 👊🏻👊🏼👊🏽👊🏾👊🏿
+ */
+
+// Create a new canvas
+let canvas = Canvas(width: 400, height: 600)
+
+// COLORS
+let lightBlue = Color(hue: 193, saturation: 100, brightness: 86, alpha: 100)
+let offWhite = Color(hue: 85, saturation: 8, brightness: 88, alpha: 100)
+let translucentPink = Color(hue: 325, saturation: 100, brightness: 81, alpha: 75)
+let black = Color(hue: 0, saturation: 100, brightness: 0, alpha: 100)
+
+// Begin your solution here...
+
+
+
+/*:
+ ## Use Source Control
+ 
+ To create evidence that supports your case for exceeding expectations for thread 3:
+
+ * You *must* commit and push regularly ... *not* just at the end of this task.
+
+ * You *must* complete your work prior to 1:00 PM on Friday, March 6, 2020.
+
+ ## Template code
+ The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
+ */
+PlaygroundPage.current.liveView = canvas
+
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Gig Poster – Task 6
+
+ To create evidence that supports your case for exceeding expectations for thread 3:
+
+ * You *must* commit and push regularly ... *not* just at the end of this task.
+
+ * You *must* complete your work prior to 1:00 PM on Friday, March 6, 2020.
+ 
+ Good luck! You've got this! 👊🏻👊🏼👊🏽👊🏾👊🏿
